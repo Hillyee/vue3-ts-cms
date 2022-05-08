46435025afb6e86bdd600d0b2ae57aac5c352b68
Hillyee <1121964992@qq.com>
2022-05-08 12:52:45 +0800

    update

--- src/base-ui/echart/hooks/useEchart.ts
+++ src/base-ui/echart/hooks/useEchart.ts
@@ -1,7 +1,7 @@
 import * as echarts from 'echarts'
 import chinaMapData from '../data/china.json'
 
-echarts.registerMap('china', chinaMapData)
+echarts.registerMap('china', chinaMapData as any)
 
 export default function (el: HTMLElement) {
   const echartsInstance = echarts.init(el)
